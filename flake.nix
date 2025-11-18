3841caeaa3c40eff4b83c59733a45d6566ba85df
qmi <minh.pham1126@gmail.com>
2025-11-18 22:16:13 +0700

    initial commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    ,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python313
+            python313Packages.numpy
+          ];
+
+          shellHook = '''';
+        };
+      }
+    );
+}
